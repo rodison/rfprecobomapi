2ca588c979ca03aa1cdff15753367936e5e9535d
rodisonferreira <rodison.ferreira@jexperts.com.br>
2022-08-31 22:47:19 -0300

    initial commit

---
+++ src/main/java/br/com/rodison/rfprecobom/rfprecobomapi/RfprecobomapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodison.rfprecobom.rfprecobomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfprecobomapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfprecobomapiApplication.class, args);
+	}
+
+}
